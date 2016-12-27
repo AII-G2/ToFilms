5e8e80f632d1c4b6821e5ac5041711b3f60e73ff
Jose <me@josearodriguez.me>
2016-12-27 22:40:54 +0100

    Creacion de los modelos

--- ToFilms/aplication/admin.py
+++ ToFilms/aplication/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from aplication.models import Director,Actor,Pelicula,Torrent
 # Register your models here.
+
+admin.site.register(Director)
+admin.site.register(Actor)
+admin.site.register(Pelicula)
+admin.site.register(Torrent)
--- ToFilms/aplication/models.py
+++ ToFilms/aplication/models.py
@@ -2,4 +2,35 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+
+
+class Actor(models.Model):
+    nombre = models.CharField(max_length=150)
+
+
+class Director(models.Model):
+    nombre = models.CharField(max_length=150)
+
+
+class Pelicula(models.Model):
+    titulo = models.CharField(max_length=150)
+    url_imagen = models.URLField()
+    valoracion_media = models.DecimalField(max_digits=3, decimal_places=2)
+    votaciones_totales = models.IntegerField()
+    anyo = models.IntegerField();
+    duracion = models.IntegerField();
+    pais = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    sinopsis = models.TextField()
+
+    actores = models.ManyToManyField(Actor)
+    directores = models.ManyToManyField(Director)
+
+
+class Torrent(models.Model):
+    calidad = models.CharField(max_length=100)
+    url = models.URLField()
+
+    pelicula = models.ForeignKey(Pelicula, on_delete=models.CASCADE)
